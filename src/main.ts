243023dc902c8afb59566d139981b0047c00365c
Viktor <ogivita11q@gmail.com>
2024-10-11 16:43:48 +0300

    __

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
   app.enableCors({
     origin: ['https://telegram-auth-frontend.vercel.app/'],
     credentials: true,
   });
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
